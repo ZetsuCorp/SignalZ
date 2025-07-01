62a7078756c11f799819da751bb1ad54b7045241
Artworqq <keysub.kevin@gmail.com>
2025-07-01 04:21:58 -0400

    Create WorldFeed.tsx

---
+++ WorldFeed.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function WorldFeed() {
+  return (
+    <div className="bg-white rounded-lg p-6 shadow text-center text-gray-500">
+      <p>WorldFeed coming soon...</p>
+    </div>
+  );
+}
