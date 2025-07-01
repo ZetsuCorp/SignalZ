4bdf7658d777939e198bb904b1eb12e0033b63c5
Artworqq <keysub.kevin@gmail.com>
2025-07-01 13:18:44 -0400

    Update WorldFeed.tsx

--- WorldFeed.tsx
+++ WorldFeed.tsx
@@ -1,10 +1,14 @@
-//WorldFeed
+// WorldFeed.tsx
 import React from "react";
 
-export default function WorldFeed() {
+interface Props {
+  wallType: string;
+}
+
+export default function WorldFeed({ wallType }: Props) {
   return (
     <div className="bg-white rounded-lg p-6 shadow text-center text-gray-500">
-      <p>WorldFeed coming soon...</p>
+      <p>WorldFeed for <strong>{wallType}</strong> wall coming soon...</p>
     </div>
   );
 }
