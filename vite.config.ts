33c25d1cfa42cefeb26904d1b0982c325bbcb410
Artworqq <keysub.kevin@gmail.com>
2025-07-01 17:54:33 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,6 @@
-// vite.config.ts
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',              // This makes sure Vite starts from project root
-  build: {
-    outDir: 'dist',       // Default Vite build output
-  },
-})
+});
