89ebcf415155e87efd3cb909246c04351c60c3b9
Artworqq <keysub.kevin@gmail.com>
2025-07-02 22:15:43 -0400

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,1 +1,9 @@
 
+<div className="app-container" style={{ display: "flex", height: "100vh", overflow: "hidden" }}>
+  <div className="left-panel">
+    {/* content here will scroll if too tall */}
+  </div>
+  <div className="right-panel" style={{ flex: 1, overflowY: "auto" }}>
+    {/* main content */}
+  </div>
+</div>
