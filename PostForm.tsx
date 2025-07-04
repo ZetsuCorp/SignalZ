daed390991d2e1cde2875006ba15836f774229f6
Artworqq <keysub.kevin@gmail.com>
2025-07-04 00:19:00 -0400

    Update PostForm.tsx

--- PostForm.tsx
+++ PostForm.tsx
@@ -56,7 +56,7 @@ function PostForm() {
 
     const imageUrl = await uploadImage();
 
-    await fetch("/.netlify/functions/create-post", {
+    await fetch("/.netlify/functions/create-posts", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
