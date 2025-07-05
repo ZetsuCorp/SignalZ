dea0944da54dda3b878bf91ea8e42298418fae4f
Artworqq <keysub.kevin@gmail.com>
2025-07-05 19:48:08 -0400

    Update fetchGoogleNews.js

--- utils/fetchGoogleNews.js
+++ utils/fetchGoogleNews.js
@@ -1,10 +1,1 @@
-export async function fetchGoogleNews(topic = "technology") {
-  try {
-    const res = await fetch(`/.netlify/functions/get-news?q=${encodeURIComponent(topic)}`);
-    if (!res.ok) throw new Error("Failed to fetch news");
-    return await res.json();
-  } catch (err) {
-    console.error("Google News fetch error:", err);
-    return [];
-  }
-}
+
