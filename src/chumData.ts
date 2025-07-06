e411466fae9a8311262fdbb3f6ae0bf0c7bf3da3
Artworqq <keysub.kevin@gmail.com>
2025-07-06 18:07:05 -0400

    Update chumData.ts

--- src/chumData.ts
+++ src/chumData.ts
@@ -1,6 +1,6 @@
 const chumPosts = [
   {
-    image_url: "https://xyz.supabase.co/storage/v1/object/public/chum/image1.png",
+    image_url: "https://lh3.googleusercontent.com/pw/AP1GczMFA_anayi0jXofshLv8WjGt5usSMPadnoldYXvqso9-cGkwmlE1E-3qH-dgDP5Dz2Cc160vRAZzEGeUWE45Fp8WoJ3QjyW7GZmKmb5BcSKMuHpp_NLyJtu3YRv2XEJT-x9FGk6mxyKOG6UlHmG8_q0AA=w551-h827-s-no-gm?authuser=0",
     caption: "This feels illegal to look at.",
     cta_url: "https://link.com/product1"
   },
