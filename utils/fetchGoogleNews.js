a4567000a5b592907119a8a89c40d57c0ed41d3e
Artworqq <keysub.kevin@gmail.com>
2025-07-05 20:42:28 -0400

    Update fetchGoogleNews.js

--- utils/fetchGoogleNews.js
+++ utils/fetchGoogleNews.js
@@ -1,1 +1,11 @@
 
+module.exports = async function fetchGoogleNews() {
+  return [
+    {
+      title: "SignalZ Launches Beta",
+      link: "https://example.com/article",
+      pubDate: new Date().toISOString(),
+      source: "SignalZ News",
+    },
+  ];
+};
