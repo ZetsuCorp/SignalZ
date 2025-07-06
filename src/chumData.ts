08b9dcf091caf397c0a45e71b864879b26c77582
Artworqq <keysub.kevin@gmail.com>
2025-07-06 14:01:02 -0400

    Create chumData.ts

---
+++ src/chumData.ts
@@ -0,0 +1,59 @@
+const chumPosts = [
+  {
+    image_url: "https://xyz.supabase.co/storage/v1/object/public/chum/image1.png",
+    caption: "This feels illegal to look at.",
+    cta_url: "https://link.com/product1"
+  },
+  {
+    image_url: "https://xyz.supabase.co/storage/v1/object/public/chum/image2.png",
+    caption: "Why would someone invent this?",
+    cta_url: "https://link.com/product2"
+  },
+   {
+    image_url: "https://xyz.supabase.co/storage/v1/object/public/chum/image2.png",
+    caption: "Why would someone invent this?",
+    cta_url: "https://link.com/product2"
+  },
+   {
+    image_url: "https://xyz.supabase.co/storage/v1/object/public/chum/image2.png",
+    caption: "Why would someone invent this?",
+    cta_url: "https://link.com/product2"
+  },
+   {
+    image_url: "https://xyz.supabase.co/storage/v1/object/public/chum/image2.png",
+    caption: "Why would someone invent this?",
+    cta_url: "https://link.com/product2"
+  },
+   {
+    image_url: "https://xyz.supabase.co/storage/v1/object/public/chum/image2.png",
+    caption: "Why would someone invent this?",
+    cta_url: "https://link.com/product2"
+  },
+   {
+    image_url: "https://xyz.supabase.co/storage/v1/object/public/chum/image2.png",
+    caption: "Why would someone invent this?",
+    cta_url: "https://link.com/product2"
+  },
+   {
+    image_url: "https://xyz.supabase.co/storage/v1/object/public/chum/image2.png",
+    caption: "Why would someone invent this?",
+    cta_url: "https://link.com/product2"
+  },
+   {
+    image_url: "https://xyz.supabase.co/storage/v1/object/public/chum/image2.png",
+    caption: "Why would someone invent this?",
+    cta_url: "https://link.com/product2"
+  },
+   {
+    image_url: "https://xyz.supabase.co/storage/v1/object/public/chum/image2.png",
+    caption: "Why would someone invent this?",
+    cta_url: "https://link.com/product2"
+  },
+   {
+    image_url: "https://xyz.supabase.co/storage/v1/object/public/chum/image2.png",
+    caption: "Why would someone invent this?",
+    cta_url: "https://link.com/product2"
+  },
+];
+
+export default chumPosts;
