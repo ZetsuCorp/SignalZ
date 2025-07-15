197efb3edcf6110ba5fb64f12bc3f170d1ff8656
Artworqq <keysub.kevin@gmail.com>
2025-07-15 06:21:03 -0400

    Update PostForm.tsx

--- PostForm.tsx
+++ PostForm.tsx
@@ -98,7 +98,7 @@ function PostForm({ wallType, onMediaPreview }) {
         sigicon_url: sigIcon, // 🔥 Attached signal icon
         wall_type: wallType,
         background,
-        display_name
+        display_name: displayname
       }),
     });
 
@@ -136,7 +136,7 @@ function PostForm({ wallType, onMediaPreview }) {
           video_url: null,
           cta_link_url: domain,
           background,
-          display_name
+          display_name: displayname
         }),
       });
 
