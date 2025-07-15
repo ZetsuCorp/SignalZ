65adf120ace7f69c3d47ea7b2e7476c045207049
Artworqq <keysub.kevin@gmail.com>
2025-07-15 17:11:48 -0400

    Update getBackgroundFromSession.js

--- src/utils/getBackgroundFromSession.js
+++ src/utils/getBackgroundFromSession.js
@@ -1,7 +1,7 @@
 export const getBackgroundFromSession = (sessionId) => {
-  const totalImages = 4; // Update this number if you add more images
+  const totalImages = 4;
   const index = sessionId
     ? parseInt(sessionId.slice(-1), 16) % totalImages
     : 0;
-     return `test${index}.png`;
+  return `/postcard-assets/cardbase/test${index}.png`;
 };
