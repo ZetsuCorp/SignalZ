228ba4bde56776399982250c2dc032d69befac34
Artworqq <keysub.kevin@gmail.com>
2025-07-15 17:56:48 -0400

    Update getBackgroundFromSession.js

--- src/utils/getBackgroundFromSession.js
+++ src/utils/getBackgroundFromSession.js
@@ -3,5 +3,5 @@ export const getBackgroundFromSession = (sessionId) => {
   const index = sessionId
     ? parseInt(sessionId.slice(-1), 16) % totalImages
     : 0;
-     return `background${index}.png`;
+     return `session_bg"${index}.png`;
 };
