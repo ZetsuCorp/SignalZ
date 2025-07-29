14b4b6a4e981dc531844c171bb005397c9191cf1
Artworqq <keysub.kevin@gmail.com>
2025-07-29 00:13:25 -0400

    Create   PanelPostView.tsx

---
+++ components/PanelPostView.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PostcardViewer from "../tcg-template/PostcardViewer";
+import PostStatsView from "../src/PostStatsView";
+import ChumFeedPanel from "../src/ChumFeedPanel";
+
+export default function PanelPostView() {
+  return (
+    <div className="left-feed">
+      <PostcardViewer />
+      <PostStatsView />
+      <ChumFeedPanel />
+    </div>
+  );
+}
