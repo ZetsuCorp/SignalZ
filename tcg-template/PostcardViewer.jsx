4a0b2ef7d5ec1724f3246edb167e5f0b78eb1e34
Artworqq <keysub.kevin@gmail.com>
2025-07-31 01:50:41 -0400

    Update PostcardViewer.jsx

--- tcg-template/PostcardViewer.jsx
+++ tcg-template/PostcardViewer.jsx
@@ -1,22 +1,30 @@
 import React from "react";
-import TCGCardTemplate from "./tcg-template/TCGCardTemplate";
 
 export default function PostForm() {
   return (
-    <div className="w-full max-w-3xl mx-auto p-4">
-      <TCGCardTemplate
-        headline=""
-        caption=""
-        image_url=""
-        video_url=""
-        sigicon_url=""
-        display_name=""
-        created_at=""
-        background=""
-        cta_url=""
-        likes={0}
-        comments={[]}
-      />
+    <div
+      className="w-full h-screen flex items-center justify-center"
+      style={{
+        background: "#010a0f",
+        padding: "2rem",
+      }}
+    >
+      <div
+        className="tcg-card-container"
+        style={{
+          width: "100%",
+          maxWidth: "700px",
+          minHeight: "500px",
+          border: "4px solid #00f0ff88",
+          borderRadius: "20px",
+          background: "#02151d",
+          boxShadow: "0 0 20px #00f0ff55",
+          padding: "2rem",
+          position: "relative",
+        }}
+      >
+        {/* TCG card will be mounted here later */}
+      </div>
     </div>
   );
 }
