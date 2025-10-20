65d617a6f2e0b0cce278394fe47c519bd78bdf59
Artworqq <keysub.kevin@gmail.com>
2025-10-20 19:36:10 -0400

    Update CreatePostShell.tsx

--- CreatePostShell.tsx
+++ CreatePostShell.tsx
@@ -288,16 +288,5 @@ export default function CreatePostShell({ mode, onClose, wallType = "main", onMe
       </div>
     </div>
   </div>
-
-    
-
-    {/* ✅ Close Button — screen bottom-right */}
-    <button
-      onClick={onClose}
-      className="absolute bottom-4 right-4 text-cyan-300 hover:text-white text-3xl font-bold z-[1000001]"
-    >
-      ✖
-    </button>
-  </div>
 );
-
+}
