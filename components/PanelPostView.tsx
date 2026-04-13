61ebcad74e3e93579e0f18c1b5ab7ab4dcb09a3e
Artworqq <keysub.kevin@gmail.com>
2026-04-13 01:41:54 -0400

    Update PanelPostView.tsx

--- components/PanelPostView.tsx
+++ components/PanelPostView.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import PostcardViewer from "../tcg-template/PostcardViewer";
 import PostStatsView from "../src/PostStatsView";
 import ChumFeedPanel from "../src/ChumFeedPanel";
-
-export default function PanelPostView() {
+ 
+export default function PanelPostView({ onPushToWall }) {
   return (
     <div className="left-feed">
-      <PostcardViewer />
+      <PostcardViewer onPushToWall={onPushToWall} />
       <PostStatsView />
       <ChumFeedPanel />
     </div>
   );
 }
+ 
